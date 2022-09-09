cf35b295d4393be441b3f6d5301d6c0dcf8578d2
lvsi <15239928381@163.com>
2022-09-09 16:44:58 +0800

    feat: hooking命令支持

--- include/function.sh
+++ include/function.sh
@@ -1,1 +1,15 @@
-#!/bin/sh
+init(){
+  echo "posture working"
+}
+
+hooking(){
+  if [ $1 == "" ]; then
+    echo "miss the directory param"
+    exit 1
+  fi
+
+  projectDir=$1
+  git config --global core.hooksPath ${projectDir}/hooks
+  chmod a+x ${projectDir}/hooks/pre-push
+  # 如果个别项目不需要使用全局的Hook, 可以在项目的根目录下重新配置git hooksPath: git config core.hooksPath .git/hooks
+}
