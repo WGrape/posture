8891de12474ed92f3b03310392dbdcd3513394f0
lvsi <15239928381@163.com>
2022-09-09 18:03:19 +0800

    feat: 优化进度条功能

--- include/function.sh
+++ include/function.sh
@@ -13,3 +13,16 @@ hooking(){
   chmod a+x ${projectDir}/hooks/pre-push
   # 如果个别项目不需要使用全局的Hook, 可以在项目的根目录下重新配置git hooksPath: git config core.hooksPath .git/hooks
 }
+
+loading(){
+  b=''
+  i=0
+  while [ $i -le 100 ]
+  do
+   printf "[%-50s] %d%% \r" "$b" "$i";
+   sleep 0.2
+   ((i=i+2))
+   b+='#'
+  done
+  echo
+}
