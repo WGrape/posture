82d282dcf8be35e9ebef9cbddb8b3f609b16a80b
lvsi <15239928381@163.com>
2022-09-13 15:54:25 +0800

    feat: 添加commit-msg钩子

--- include/function.sh
+++ include/function.sh
@@ -18,6 +18,8 @@ hooking(){
   projectDir=$1
   git config --global core.hooksPath ${projectDir}/hooks
   chmod a+x ${projectDir}/hooks/pre-push
+  chmod a+x ${projectDir}/hooks/pre-commit
+  chmod a+x ${projectDir}/hooks/commit-msg
   # 如果个别项目不需要使用全局的Hook, 可以在项目的根目录下重新配置git hooksPath: git config core.hooksPath .git/hooks
 }
 
