461ab2396f9e3b216d71b413ad8f7222d3ee30ca
lvsi <15239928381@163.com>
2024-02-28 17:05:12 +0800

    update: update function.sh style

--- include/function.sh
+++ include/function.sh
@@ -1,85 +1,7 @@
 #!/bin/sh
 . $POSTUREPATH/config/config.sh
 
-# 开始调整
-adjust(){
-  echo "正在调整中 ... (posture is adjusting ...)"
-  echo "> 配置的语言为${lang} (your lang configuration is ${lang})"
-  git config --global --add core.fileMode false
-  cp $POSTUREPATH/include/editor/${lang}/.editorconfig ./
-  echo "> 完成 (done)"
-}
-
-# 设置全局Hook
-hook(){
-  if [ $1 == "" ]; then
-    echo "缺少目录参数 (miss the directory param)"
-    exit 1
-  fi
-
-  projectDir=$1
-  git config --global core.hooksPath ${projectDir}/include/hooks
-  chmod a+x ${projectDir}/include/hooks/commit-msg
-  chmod a+x ${projectDir}/include/hooks/post-checkout
-  chmod a+x ${projectDir}/include/hooks/post-merge
-  chmod a+x ${projectDir}/include/hooks/pre-commit
-  chmod a+x ${projectDir}/include/hooks/pre-push
-
-  # 如果个别项目不需要使用全局的Hook, 可以在项目的根目录下重新配置git hooksPath: git config core.hooksPath .git/hooks
-  echo "If your other projects don't need require the global Hooks, they can be reconfigured in the root directory of your other projects: git config core.hooksPath .git/hooks"
-}
-
-# 取消设置全局Hook
-unhook(){
-  git config --global core.hooksPath false
-}
-
-# 检查配置
-check_config(){
-  check_lang
-  if [ $? -ne 0 ]; then
-      echo "lang配置错误: ${lang} (configuration of lang is error: ${lang})"
-      exit 1
-  fi
-}
-
-# 更新版本
-update_version(){
-  currentDir=$(pwd)
-  cd $POSTUREPATH && git pull && cd $currentDir
-  # 判断是否拉取成功
-  if [ $? -ne 0 ]; then
-    echo "版本更新至最新版本失败 (Failed to update the version to the latest version)"
-    exit 1
-  fi
-  echo "版本更新至最新版本成功 (update to latest version successfully)"
-}
-
-# 显示状态信息
-status(){
-  echo "The list of configuration"
-  echo "> lang = ${lang}"
-  echo "The list of states"
-  echo "> version = ${version}"
-}
-
-# 检查配置中的编程语言
-check_lang(){
-  if [ "${lang}" == "go" ]; then
-    return 0
-  elif [ "${lang}" == "java" ]; then
-    return 0
-  elif [ "${lang}" == "js" ]; then
-    return 0
-  elif [ "${lang}" == "php" ]; then
-    return 0
-  elif [ "${lang}" == "shell" ]; then
-    return 0
-  fi
-  return 1
-}
-
-# 打印logo
+# print logo
 print_logo(){
   echo "\033[34m
 ==========================================================================
@@ -94,33 +16,7 @@ print_logo(){
 \033[0m"
 }
 
-# 发送钉钉消息
-send_ding_message(){
-  message=$1
-  if [ "${message}" == "" ]; then
-    echo "钉钉消息不能为空 (The DingDing message must not be empty)"
-    return 0
-  fi
-  if [ "${dingToken}" == "" ]; then
-      echo "钉钉Token不能为空 (The DingDing token must not be empty)"
-      return 0
-    fi
-  curl -H 'Content-type: application/json' -d "{\"msgtype\":\"text\", \"text\": {\"content\":\"${message}\"}}" "https://oapi.dingtalk.com/robot/send?access_token=${dingToken}"
-}
-
-# 记录commit日志
-write_commit_log(){
-  day=$(date "+%Y%m%d")
-  datetime=$(date "+%Y-%m-%d/%H:%M:%S")
-  projectName=$(git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//')
-  commitUser=$1
-  commitBranch=$2
-  commitId=$3
-  commitMsg=$4
-  echo "${datetime} ${projectName} ${commitUser} ${commitBranch} ${commitId} ${commitMsg}" >> $POSTUREPATH/storage/commit.${day}.log
-}
-
-# 加载中
+# loading
 loading(){
   sleepSeconds=0.2
   if [ "${loadingSpeed}" == 1 ]; then
@@ -145,4 +41,113 @@ loading(){
    b+='#'
   done
   echo
+}
+
+# send web hook message
+send_ding_message(){
+  message=$1
+  if [ "${message}" == "" ]; then
+    echo "钉钉消息不能为空 (The DingDing message must not be empty)"
+    return 0
+  fi
+  if [ "${dingToken}" == "" ]; then
+      echo "钉钉Token不能为空 (The DingDing token must not be empty)"
+      return 0
+    fi
+  curl -H 'Content-type: application/json' -d "{\"msgtype\":\"text\", \"text\": {\"content\":\"${message}\"}}" "https://oapi.dingtalk.com/robot/send?access_token=${dingToken}"
+}
+
+# write commit log
+write_commit_log(){
+  day=$(date "+%Y%m%d")
+  datetime=$(date "+%Y-%m-%d/%H:%M:%S")
+  projectName=$(git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//')
+  commitUser=$1
+  commitBranch=$2
+  commitId=$3
+  commitMsg=$4
+  echo "${datetime} ${projectName} ${commitUser} ${commitBranch} ${commitId} ${commitMsg}" >> $POSTUREPATH/storage/commit.${day}.log
+}
+
+# check the language.
+check_lang(){
+  if [ "${lang}" == "go" ]; then
+    return 0
+  elif [ "${lang}" == "java" ]; then
+    return 0
+  elif [ "${lang}" == "js" ]; then
+    return 0
+  elif [ "${lang}" == "php" ]; then
+    return 0
+  elif [ "${lang}" == "shell" ]; then
+    return 0
+  fi
+  return 1
+}
+
+# check the config
+check_config(){
+  check_lang
+  if [ $? -ne 0 ]; then
+      echo "lang配置错误: ${lang} (configuration of lang is error: ${lang})"
+      exit 1
+  fi
+}
+
+# start to adjust
+adjust(){
+  if ! check_config; then
+      echo "adjust failed: check_config"
+      exit 1
+  fi
+
+  echo "正在调整中 ... (posture is adjusting ...)"
+  echo "> 配置的语言为${lang} (your lang configuration is ${lang})"
+  git config --global --add core.fileMode false
+  cp $POSTUREPATH/include/editor/${lang}/.editorconfig ./
+  echo "> 完成 (done)"
+}
+
+# start to set the global hook
+hook(){
+  if [ $1 == "" ]; then
+    echo "缺少目录参数 (miss the directory param)"
+    exit 1
+  fi
+
+  projectDir=$1
+  git config --global core.hooksPath ${projectDir}/include/hooks
+  chmod a+x ${projectDir}/include/hooks/commit-msg
+  chmod a+x ${projectDir}/include/hooks/post-checkout
+  chmod a+x ${projectDir}/include/hooks/post-merge
+  chmod a+x ${projectDir}/include/hooks/pre-commit
+  chmod a+x ${projectDir}/include/hooks/pre-push
+
+  # 如果个别项目不需要使用全局的Hook, 可以在项目的根目录下重新配置git hooksPath: git config core.hooksPath .git/hooks
+  echo "If your other projects don't need require the global Hooks, they can be reconfigured in the root directory of your other projects: git config core.hooksPath .git/hooks"
+}
+
+# start to cancel the global hook
+unhook(){
+  git config --global core.hooksPath false
+}
+
+# update your posture version
+update_version(){
+  currentDir=$(pwd)
+  cd $POSTUREPATH && git pull && cd $currentDir
+  # 判断是否拉取成功
+  if [ $? -ne 0 ]; then
+    echo "版本更新至最新版本失败 (Failed to update the version to the latest version)"
+    exit 1
+  fi
+  echo "版本更新至最新版本成功 (update to latest version successfully)"
+}
+
+# show status
+status(){
+  echo "The list of configuration"
+  echo "> lang = ${lang}"
+  echo "The list of states"
+  echo "> version = ${version}"
 }
